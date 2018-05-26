9fa6aaa5e7eaa328534fcd9c897a7d28b18c17f2
Bill Grant <bill@billgrant.io>
2018-05-26 16:44:53 -0400

    Added lamba upload script

---
+++ upload-portfolio-lambda.py
@@ -0,0 +1,20 @@
+import boto3
+import StringIO
+import zipfile
+import mimetypes
+
+s3 = boto3.resource('s3')
+
+portfolio_bucket = s3.Bucket('billgrant.io')
+build_bucket = s3.Bucket('portfoliobuild.billgrant.io')
+
+portfolio_zip = StringIO.StringIO()
+build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+
+with zipfile.ZipFile(portfolio_zip) as myzip:
+    for nm in myzip.namelist():
+        obj = myzip.open(nm)
+        mime_type = mimetypes.guess_type(nm)[0]
+        portfolio_bucket.upload_fileobj(obj, nm, 
+            ExtraArgs={'ContentType': str(mime_type)})
+        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
