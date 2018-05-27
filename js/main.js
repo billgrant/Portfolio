16e699b29f7d061c9439bae71687a6d3c79b83e9
Bill Grant <bill@billgrant.io>
2018-05-27 14:34:12 -0400

    Moved work examples to react

--- js/main.js
+++ js/main.js
@@ -1,5 +1,32 @@
-import 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleWork from './example-work';
 
-console.log("Loaded react-dom");
+const myWork = [
+  {
+    'title': "Automation",
+    'image': {
+      'desc': "Screenshot of a project involving code",
+      'src': "images/example1.png",
+      'comment': `"changehostnames.py"`
+    }
+  },
+  {
+    'title': "Cloud Architecture",
+    'image': {
+      'desc': "Screenshot of a project involving cloud",
+      'src': "images/example2.png",
+      'comment': `"VPC Peering"`
+    }
+  },
+  {
+    'title': "Cloud Architecture",
+    'image': {
+      'desc': "Screenshot of a project involving infrastructure",
+      'src': "images/example3.png",
+      'comment': `"Singapore"`
+    }
+  }
+]
 
-console.log("Webpack works");
+ReactDOM.render(<ExampleWork work={myWork} />, document.getElementById('example-work'));
